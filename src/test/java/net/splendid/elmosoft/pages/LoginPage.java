65e5977e2f9b258e2cc2b237cdd7e5323ad783dd
Tanya Tarasovec <tanyashipicina300197@gmail.com>
2022-01-19 18:57:57 +0300

    Authorization test

---
+++ src/test/java/net/splendid/elmosoft/pages/LoginPage.java
@@ -0,0 +1,47 @@
+package net.splendid.elmosoft.pages;
+
+import net.elmosoft.splendid.driver.annotation.FindBy;
+import net.elmosoft.splendid.driver.element.BrowserElement;
+import net.elmosoft.splendid.driver.page.Page;
+import net.elmosoft.splendid.driver.page.PageFactory;
+
+public class LoginPage extends Page {
+
+    @FindBy(id = "user-name")
+    private BrowserElement loginField;
+
+    @FindBy(id = "password")
+    private BrowserElement passwordField;
+
+    @FindBy(id = "login-button")
+    private BrowserElement loginBtn;
+
+    public HomePage dologin(String username, String password) {
+        loginField.type(username);
+        passwordField.type(password);
+        loginBtn.click();
+        return PageFactory.initElements(driver, HomePage.class);
+
+
+    }
+
+    public LoginPage openPage() {
+        driver.get("https://www.saucedemo.com/");
+        return PageFactory.initElements(driver, LoginPage.class);
+
+    }
+
+
+    @Override
+    public void checkPage() {
+        waitForPageIsLoaded();
+        loginBtn.waitForElementDisplayed(TEN_SECONDS_WAIT);
+
+    }
+
+    @Override
+    public void waitForPageIsLoaded() {
+        driver.waitForPageLoad();
+
+    }
+}
---
+++ src/test/java/net/splendid/elmosoft/tests/AuthorizationTest.java
@@ -0,0 +1,30 @@
+package net.splendid.elmosoft.tests;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import net.elmosoft.splendid.service.runner.SuiteListener;
+import net.elmosoft.splendid.test.BaseSplendidTest;
+import net.splendid.elmosoft.pages.HomePage;
+import net.splendid.elmosoft.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Guice;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.uncommons.reportng.HTMLReporter;
+
+@Listeners({ SuiteListener.class, HTMLReporter.class })
+@Epic("Smoke Tests")
+@Feature("Login")
+@Guice
+
+public class AuthorizationTest extends BaseSplendidTest {
+
+    @Test
+    public void verifyValidCread(){
+        LoginPage login = new LoginPage();
+        login.openPage().checkPage();
+        Boolean resultAuthorization = login.dologin("standart_user", "secret_sauce").isHomePageOpened();
+        Assert.assertTrue(resultAuthorization, "Home page is not opened");
+
+    }
+}
